080ea7e963d25488b5037f23e5f7466622f9cb0f
Stephen <stephen.okpeku@morgansconsortium.com>
2023-01-19 13:50:14 +0100

    first commit

---
+++ resources/views/newdesign/layout/changepass.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $toptitle }}</title>
+
+    <meta charset="utf-8">
+<meta name="language" content="en"/>
+<meta name="description" content="We Are The Leading And Specialist Provider Of Specialized Corporate Training Including Governance, Risk & Compliance, Financial Crime, And Leadership Training Focused On Cultivating The Right Attitude And Skills In Your Teams."/>
+<meta name="keyword" content="TMC Institute, Best Learning Management System, Affordable Price For E-Learning Platform, Learn at your comfort, Fast Track your Career, "/>
+<meta name="author" content="TMC Institute"/>
+
+<link rel="canonical" href="https://tmcinstitute.com/" />
+
+<!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1667831345/Tmc%20institute/fav_dmznt6.jpg">
+<link rel="apple-touch-icon" sizes="180x180" type="image/x-icon" href="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1667831345/Tmc%20institute/fav_dmznt6.jpg">
+<link rel="icon" type="image/png" sizes="32x32" href="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1667831345/Tmc%20institute/fav_dmznt6.jpg">
+<link rel="icon" type="image/png" sizes="16x16" href="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1667831345/Tmc%20institute/fav_dmznt6.jpg">
+
+<meta property="og:locale" content="en_US">
+<meta property="og:type" content="website">
+<meta property="og:title" content="TMC Institute | The Leading And Specialist Provider Of Specialized Corporate Training Including Governance, Risk & Compliance, Financial Crime">
+<meta property="og:description" content="We Are The Leading And Specialist Provider Of Specialized Corporate Training Including Governance, Risk & Compliance, Financial Crime, And Leadership Training Focused On Cultivating The Right Attitude And Skills In Your Teams.">
+
+<meta property="og:url" content="https://tmcinstitute.com/">
+<meta property="og:site_name" content="TMC Institute">
+
+<meta property="og:image" content="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1667831345/Tmc%20institute/fav_dmznt6.jpg">
+<meta property="og:image:width" content="1600">
+<meta property="og:image:height" content="863">
+
+
+
+<meta name="msapplication-TileColor" content="#004281">
+<meta name="theme-color" content="#ffffff">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-title" content="TMC Institute">
+<meta name="msapplication-TileImage" content="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1667831345/Tmc%20institute/fav_dmznt6.jpg">
+
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
+  {{--  <link rel="stylesheet" href="{{ asset('css/icons.css') }}"/>  --}}
+  <link rel="stylesheet" href="{{ asset('css/uikit.css') }}"/>
+  <link rel="stylesheet" href="{{ asset('css/tailwind.min.css') }}"/>
+  {{--  <link rel="stylesheet" href="{{ asset('css/icons.css') }}}}" />  --}}
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="shortcut icon" href="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1667831345/Tmc%20institute/fav_dmznt6.jpg" type="image/x-icon"/>
+
+
+</head>
+<body>
+    <article class=''>
+        <div class="preloader grid place-content-center">
+            <div class="w-60">
+                <img src="https://res.cloudinary.com/the-morgans-consortium/image/upload/v1659435270/Tmc%20institute/TMC_Institute_logo_kpv3d4.jpg" alt="preloader" class="preloader__item" />
+            </div>
+
+          </div>
+    {{--  @include('partials.navbar')  --}}
+
+    @yield('content')
+    {{--  @include('partials.footer')  --}}
+    </article>
+    </body>
+{{--  <script src="https://www.paypal.com/sdk/js?client-id=AYauehWxDIcW5N-L432YO1bcyGnqy4lzfx00pdgX5VLRlm0qQ48rKn4odTFy8J_bWdUi57eOAS8c1ato&components=buttons"></script>  --}}
+<script type="text/javascript">
+var status_type = {{ Js::from($status??"") }}
+var code  = {{ Js::from($code??"") }}
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6311da2337898912e966ce56/1gburo81e';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('js/uikit.min.js') }}"></script>
+<script src="{{ asset('js/swap.js') }}"></script>
+</html>
