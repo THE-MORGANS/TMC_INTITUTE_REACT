2860e0fb727dd3c141ef33fbd92dd576c0741259
Eshanokpe Daniel <eshanokpe@gmail.com>
2024-09-12 19:51:57 +0100

    auth

--- storage/framework/views/54c34e1680c3e458598f9402ccad8f6fc7cf806f.php
+++ storage/framework/views/54c34e1680c3e458598f9402ccad8f6fc7cf806f.php
@@ -3,5 +3,5 @@
 
     </div>
 <?php $__env->stopSection(); ?> 
-
+ 
 <?php echo $__env->make('newdesign.layout.newindex', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/infosert/Documents/laravel_projects/TMC_INTITUTE_REACT/resources/views/newdesign/index.blade.php ENDPATH**/ ?>
