f29d228654c3315f76c246a3b437dc9a3c12970f
infoscert <eshanokpe@gmail.com>
2026-06-12 03:31:48 +0100

    chore: update app

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,7 +6,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
+     
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
@@ -18,7 +18,7 @@ createInertiaApp({
         const root = createRoot(el);
 
         root.render(<App {...props} />);
-    },
+    },  
     progress: {
         color: '#4B5563',
     },
